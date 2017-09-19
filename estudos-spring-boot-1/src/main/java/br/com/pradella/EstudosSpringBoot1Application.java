f39f4b0af336119d0d1e1079199379ae99cef452
Lucas <lucas.pradella@hotmail.com>
2017-09-19 17:59:01 -0300

    Adicionando scanBasePackages.

--- estudos-spring-boot-1/src/main/java/br/com/pradella/EstudosSpringBoot1Application.java
+++ estudos-spring-boot-1/src/main/java/br/com/pradella/EstudosSpringBoot1Application.java
@@ -3,7 +3,7 @@ package br.com.pradella;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="br.com.pradella")
 public class EstudosSpringBoot1Application {
 
 	public static void main(String[] args) {
