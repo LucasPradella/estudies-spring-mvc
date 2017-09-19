8c22992afff584a47b90562edee1ff7add0e58ef
Lucas <lucas.pradella@hotmail.com>
2017-09-19 17:54:39 -0300

    Alterando pacotes, para mapeamento correto do controller

--- estudos-spring-boot-1/src/main/java/br/com/pradella/EstudosSpringBoot1Application.java
+++ estudos-spring-boot-1/src/main/java/br/com/pradella/EstudosSpringBoot1Application.java
@@ -1,4 +1,4 @@
-package br.com.pradella.exemplo;
+package br.com.pradella;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
