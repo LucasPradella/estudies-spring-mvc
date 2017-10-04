7d02f01250bc11dd1b4e7f79f3c2eec290cb19a8
Lucas <lucas.pradella@hotmail.com>
2017-10-04 11:55:16 -0300

    Adicionando dependencias necessarias + application.properties

---
+++ estudos-spring-boot-1/src/main/java/br/com/pradella/testes/ValorProperties.java
@@ -0,0 +1,18 @@
+package br.com.pradella.testes;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class ValorProperties {
+
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina ;
+	
+	
+	
+	public int getQtdPorPagina() {
+		return qtdPorPagina;
+	}
+	
+
+}
---
+++ estudos-spring-boot-1/src/main/java/br/com/pradella/testes/ValorPropertiesMain.java
@@ -0,0 +1,19 @@
+package br.com.pradella.testes;
+
+public class ValorPropertiesMain {
+
+	
+	
+		public static void main(String[] args) {
+	
+		 ValorProperties valorProperties = new ValorProperties();
+		 
+		 	System.out.println(valorProperties.getQtdPorPagina());
+			
+		}
+	
+	
+		
+	
+
+}
