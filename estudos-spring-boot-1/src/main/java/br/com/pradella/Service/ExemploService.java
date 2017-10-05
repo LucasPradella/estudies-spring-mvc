1eba97d615c244c726e6d490e13bad7d2facc7b0
Lucas <lucas.pradella@hotmail.com>
2017-10-05 10:29:04 -0300

    Adicionando Swagger.

---
+++ estudos-spring-boot-1/src/main/java/br/com/pradella/Service/ExemploService.java
@@ -0,0 +1,12 @@
+package br.com.pradella.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExemploService {
+
+	private void testarService() {
+			System.out.println("#### Executando serviço de teste ! ####");
+	}
+	
+}
---
+++ estudos-spring-boot-1/src/main/java/br/com/pradella/controllers/EmpresaController.java
@@ -0,0 +1,55 @@
+package br.com.pradella.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pradella.DTO.EmpresaDTO;
+import br.com.pradella.Response.API.Response;
+
+@RestController
+@RequestMapping("/api/empresas")
+public class EmpresaController {
+	
+	
+/*	
+ * Sem tratar o retorno 
+ * 
+ * 
+ * @PostMapping
+	public ResponseEntity<EmpresaDTO> cadastrar(@RequestBody EmpresaDTO empresaDTO) {
+		empresaDTO.setId(1L);
+		return ResponseEntity.ok(empresaDTO);
+	}*/
+
+	@PostMapping
+	public ResponseEntity<Response<EmpresaDTO>> cadastrar(@Valid @RequestBody EmpresaDTO empresaDTO, BindingResult result) {
+		
+		Response<EmpresaDTO> response = new Response<EmpresaDTO>();
+		
+		
+		if (result.hasErrors()) {
+			List<String> collect = result.getAllErrors().stream()
+															.map(error -> error.getDefaultMessage())
+															.collect(Collectors.toList());
+			
+			response.setErrors(collect);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		empresaDTO.setId(1L);
+		response.setData(empresaDTO);
+		return ResponseEntity.ok(response);
+		
+	}
+	
+	
+}
