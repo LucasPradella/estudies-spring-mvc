368a7c544897c096bc2685962450dacc3de307e1
Lucas <lucas.pradella@hotmail.com>
2017-11-08 11:08:15 -0200

    Adicionando Adicionar + adicionando comunicação com o banco de dados + adicionando spring security.

--- estudos-spring-boot-web/src/main/java/br/com/pradella/Controller/ConvidadosController.java
+++ estudos-spring-boot-web/src/main/java/br/com/pradella/Controller/ConvidadosController.java
@@ -3,6 +3,8 @@ package br.com.pradella.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.pradella.model.Convidado;
@@ -10,6 +12,7 @@ import br.com.pradella.repository.Convidados;
 
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	
@@ -17,19 +20,29 @@ public class ConvidadosController {
 	private Convidados convidados;
 	
 	
-	@GetMapping("/convidados")
+	/*@GetMapping("/convidados") caso nao queria usar o requestMapping*/
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
-/*		modelAndView.addObject("convidados", convidados.findAll());*/
+		modelAndView.addObject("convidados", convidados.findAll());
 		
-		Convidado convidado = new Convidado();
+/*
+ * Usado para os primeiros testes ... 
+ * 		Convidado convidado = new Convidado();
 		convidado.setNome("Lucas");
 		convidado.setQuantidadeAcompanhantes(1);
-		
 		modelAndView.addObject("convidados", convidado);
+*/		
 		
-		
+		modelAndView.addObject(new Convidado());
 		return modelAndView;
+	}
+	
+	/*@PostMapping("/convidados")*/
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 
 }
